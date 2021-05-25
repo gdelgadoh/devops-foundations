0c4cd2b4ed94e2b54940aa286795f633b7eb938b
Guido Delgado H <gdelgadoh@baccredomatic.com>
2021-05-25 14:35:32 -0600

    Commit inicial

---
+++ src/main/java/com/dachshundcompany/devopsfoundations/DevopsFoundationsApplication.java
@@ -0,0 +1,13 @@
+package com.dachshundcompany.devopsfoundations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevopsFoundationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevopsFoundationsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dachshundcompany/devopsfoundations/DevopsFoundationsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dachshundcompany.devopsfoundations;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevopsFoundationsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
